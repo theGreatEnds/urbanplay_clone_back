9b24bab7c20776d2f2defb5197f22640473525dd
Donggyu Kim <domdom2y2@gmail.com>
2022-06-09 19:42:02 +0900

    frontend init file

--- restapi/settings.py
+++ restapi/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'authentication',
     'rest_framework',
+    'frontend'
 ]
 
 REST_FRAMEWORK = {
--- restapi/urls.py
+++ restapi/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authentication.urls'))
+    path('api/', include('authentication.urls')),
+    path('', include('frontend.urls')) # frontend urls.py 별도 관리
 ]
