db892e88be8797ff09297fe2b59550d97ec95311
Donggyu Kim <domdom2y2@gmail.com>
2022-06-09 20:29:26 +0900

    frontend 제거
    frontend 에서 따로 관리할 예정이기 때문에 backend 에서는 제거합니다.

--- frontend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import index
-
-urlpatterns = [
-    path('', index),  # for the empty url
-    path(r'.*/', index)  # for all other urls
-]
--- frontend/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request): # react index.js rendering
-    return render(request, 'frontend/index.html', context=None)
