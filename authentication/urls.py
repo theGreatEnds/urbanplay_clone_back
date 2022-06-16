18cfc63c2904a7c2be60adc5b3aa727710c0d371
Donggyu Kim <domdom2y2@gmail.com>
2022-06-17 02:05:40 +0900

    jwt token blacklist 추가
    front에서는 로그아웃할 때 해당 API 호출하면 됨

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import ObtainTokenPairWithUserInfoView, CustomUserCreate, HelloWorldView
+from .views import ObtainTokenPairWithUserInfoView, CustomUserCreate, HelloWorldView, LogoutAndBlacklistRefreshTokenForUserView
 
 urlpatterns = [
     path('user/create/', CustomUserCreate.as_view(), name="create_user"),
     path('token/obtain/', ObtainTokenPairWithUserInfoView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 	path('test/', HelloWorldView.as_view(), name='hello_world'),
+    path('blacklist/', LogoutAndBlacklistRefreshTokenForUserView.as_view(), name='blacklist'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,5 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,6 +9,18 @@ from .serializers import MyTokenObtainPairSerializer, CustomUserSerializer
 class ObtainTokenPairWithUserInfoView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
+class LogoutAndBlacklistRefreshTokenForUserView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 class CustomUserCreate(APIView):
     permission_classes = (permissions.AllowAny,)
