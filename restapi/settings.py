633cfe313cd73c362e68e952c456b2e81eb8a093
Donggyu Kim <domdom2y2@gmail.com>
2022-06-30 21:25:32 +0900

    frontend image 경로 static 에 추가

--- restapi/settings.py
+++ restapi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,7 +153,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'public/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'urbanplay_clone_front/public'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
